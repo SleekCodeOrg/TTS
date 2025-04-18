d913899d36779b33bbbc06ca2958d68bf48601ad
Jiyan Jonas Schneider <jiyan@sleekcode.jp>
2025-04-18 03:00:33 +0000

    Add setup

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ from Cython.Build import cythonize
 from setuptools import Extension, find_packages, setup
 
 python_version = sys.version.split()[0]
-if Version(python_version) < Version("3.9") or Version(python_version) >= Version("3.12"):
+if Version(python_version) < Version("3.9") or Version(python_version) >= Version("3.14"):
     raise RuntimeError("TTS requires python >= 3.9 and < 3.12 " "but your Python version is {}".format(sys.version))
 
 
