037c39908e507ae256ffdddea130f12f76af503e
Jiyan Jonas Schneider <mail@jiyanjs.com>
2025-06-04 22:35:17 +0900

    also update string

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ from setuptools import Extension, find_packages, setup
 
 python_version = sys.version.split()[0]
 if Version(python_version) < Version("3.9") or Version(python_version) >= Version("3.14"):
-    raise RuntimeError("TTS requires python >= 3.9 and < 3.12 " "but your Python version is {}".format(sys.version))
+    raise RuntimeError("TTS requires python >= 3.9 and < 3.14 " "but your Python version is {}".format(sys.version))
 
 
 cwd = os.path.dirname(os.path.abspath(__file__))
